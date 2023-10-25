da51f1851b00df8698122a0ca37bd4b0bdcadfd4
Modestas Motiejunas <chiopra@gmail.com>
2023-10-25 12:57:10 +0300

    new problem with hashmaps added

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {} # hasmaps to solve it
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:                        # for keys
+            if countS[c] != countT.get(c, 0):   # if values do not match
+                return False
+
+        return True
